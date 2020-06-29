e6e3fda4e89bfb939e383c6562852e50e9b1aaf5
jbachell <john.bacheller1@gmail.com>
2020-06-29 11:03:12 -0400

    update to demo

--- tests/test_workspaces/demo.py
+++ tests/test_workspaces/demo.py
@@ -29,24 +29,26 @@ from moto.core import ACCOUNT_ID
 
 
 def test_boto3_workspaces():
-    client = boto3.client('workspaces')
+    client = boto3.client('workspaces', aws_access_key_id='id',
+                            aws_secret_access_key='secret',
+                            aws_session_token='token', region_name='us-east-1')
 
-    temp = client.create_workspaces(
-                        Workspaces=[{
-                            'DirectoryId': "d-29381asfdw",
-                            'BundleId': "wsb-asd42hfg1",
-                            'UserName': "ajanedoe1"
-                        }]
-                    )
+    # temp = client.create_workspaces(
+    #                     Workspaces=[{
+    #                         'DirectoryId': "d-29381asfdw",
+    #                         'BundleId': "wsb-asd42hfg1",
+    #                         'UserName': "ajanedoe1"
+    #                     }]
+    #                 )
 
     lst = client.describe_workspaces()
 
     print(lst)
 
-@mock_workspaces
-wrapper_func():
+#@mock_workspaces
+def wrapper_func():
     print("Calling boto3.workspaces!")
 
     test_boto3_workspaces()
 
-wrapper_fun()
+wrapper_func()
